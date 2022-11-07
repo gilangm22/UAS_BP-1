c01eac81e0c0f0e88470ff698a659401cfd2c09c
Fkrhsnn <114807566+Fkrhsnn@users.noreply.github.com>
2022-11-07 13:25:11 +0700

    Create no2.py

---
+++ no2.py
@@ -0,0 +1,11 @@
+print("MENGHITUNG LUAS SEGITIGA")
+
+# Menginput Alas dan Tinggi Segitiga
+alas = float(input('Masukan Alas: '))
+tinggi = float(input('Masukan Tinggi: '))
+ 
+# Hitung Luas Segitiga
+luas = (alas * tinggi) / 2
+ 
+#Menampilkan Hasil Perhitungan
+print('Luas Segitiga adalah %0.2f' %luas)
