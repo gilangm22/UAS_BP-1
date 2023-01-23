697dfc76da73dd5ae98ded5888d3df58955e7b5e
Fkrhsnn <114807566+Fkrhsnn@users.noreply.github.com>
2023-01-23 12:58:10 +0700

    Delete no4.py

--- no4.py
+++
@@ -1,4 +0,0 @@
-list = [['Kalimantan West','Kalimantan North','Kalimantan South'],['Sumatra West','Sumatra North','Sumatra South',],['Sulawesi West','Sulawesi North','Sulawesi South']] # List with in the list
-for i in list:
-    for j in i:
-        print(j)
